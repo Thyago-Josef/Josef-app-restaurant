c24a40b589a81cda78aac0d976b894df4e3c3489
Thyago-Josef <t.y_josef7charm@hotmail.com>
2025-06-11 18:06:50 -0300

    Initial commit

---
+++ src/main/java/com/josef/Josef_app_restaurant/JosefAppRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.josef.Josef_app_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosefAppRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JosefAppRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josef/Josef_app_restaurant/JosefAppRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josef.Josef_app_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosefAppRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
